700bef5ee9ecb0ed8b4ead661bc98caddf48c0f0
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-04-28 15:16:12 +0200

    please don't drop headers w/license

--- src/lib/vid.py
+++ src/lib/vid.py
@@ -2,6 +2,13 @@
 This code was derived from deeplabcut.utils make_labeled_video & video_processor
 https://github.com/DeepLabCut/DeepLabCut/blob/master/deeplabcut/utils/make_labeled_video.py
 https://github.com/DeepLabCut/DeepLabCut/blob/master/deeplabcut/utils/video_processor.py
+
+DeepLabCut2.0 Toolbox (deeplabcut.org)
+© A. & M. Mathis Labs
+https://github.com/DeepLabCut/DeepLabCut
+Please see AUTHORS for contributors.
+https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
+Licensed under GNU Lesser General Public License v3.0
 """
 
 import os
