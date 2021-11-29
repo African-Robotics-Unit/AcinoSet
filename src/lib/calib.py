a2ab1e4ca967877ae7c0c05ecd5a64bfdb4eb4dc
Zico <zicods7@gmail.com>
2021-11-29 12:50:17 +0000

    Added method to project object board points to the image plane.

--- src/lib/calib.py
+++ src/lib/calib.py
@@ -135,6 +135,14 @@ def project_points_fisheye(obj_pts, k, d, r, t):
     pts =  cv.fisheye.projectPoints(obj_pts_reshaped, r_vec, t, k, d)[0].reshape((-1, 2))
     return pts
 
+def project_board_points(obj_pts, k, d, r, t):
+    obj_pts_reshaped = obj_pts.reshape((-1, 1, 3))
+    ret = []
+    for (r_vec, t_vec) in zip(r, t):
+        ret.append(cv.projectPoints(obj_pts_reshaped, r_vec, t_vec, k, d)[0].reshape((-1, 2)))
+
+    return np.asarray(ret)
+
 
 # ========== ESTIMATION ALGORITHMS ==========
 
@@ -234,7 +242,7 @@ def adjust_extrinsics_manual_points(calib_func, img_pts_arr, cam_idxs_to_correct
             cam_b_params += [r_arr[b] @ R.T, t_arr[b] - r_arr[b] @ t] if b in cam_idxs_to_correct else [r_arr[b], t_arr[b]]
 
             skew_3d_pts = triangulate_func(
-                np.array(img_pts_arr[:, a]), np.array(img_pts_arr[:, b]), 
+                np.array(img_pts_arr[:, a]), np.array(img_pts_arr[:, b]),
                 *cam_a_params, *cam_b_params
             )
 
@@ -334,7 +342,7 @@ def _calibrate_pairwise_extrinsics(
             board_edge_len = board_edge_len_1
         else:
             np.testing.assert_equal(board_edge_len, board_edge_len_1) # handles nan case
-            
+
     # Load the dummy scene
     with open(dummy_scene_fpath, 'rb') as f:
         dummy_scene_data = json.load(f)
@@ -348,7 +356,7 @@ def _calibrate_pairwise_extrinsics(
         'r': dummy_rs,
         't': dummy_ts,
     }
-    
+
     # determine cam pairs to be used in calibration
     cams = np.array([int(list(filter(str.isdigit, fpath))[-1]) for fpath in points_fpaths])
     cam_pairs = None
@@ -373,10 +381,10 @@ def _calibrate_pairwise_extrinsics(
 
     if incomplete_cams:
         from os.path import join, dirname
-        
+
         incomplete_fpath = out_fpath.replace('.json', '_before_corrections.json')
         save_scene(incomplete_fpath, k_arr, d_arr, r_arr, t_arr, cam_res)
-        
+
         if manual_points_fpath is None:
             manual_points_fpath = join(dirname(points_fpaths[0]), 'manual_points.json')
 
@@ -387,7 +395,7 @@ def _calibrate_pairwise_extrinsics(
         except FileNotFoundError as e:
             print(f'\nPlease rerun this calibration after obtaining manually-labelled points')
             raise FileNotFoundError(e)
-            
+
         cam_idxs_to_correct = list(range(cams.index(incomplete_cams[0]),len(cams)))
         r_arr, t_arr = adjust_extrinsics_manual_points(calib_func, img_pts_arr, cam_idxs_to_correct, k_arr, d_arr, r_arr, t_arr)
 
